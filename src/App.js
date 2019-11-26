4d4395a9fc4653515e2d5cdfbcec7d969882df71
jays29 <juangnr@gmail.com>
2019-11-26 13:30:30 -0300

    Data to Google Firebase migration. Redux modification. Items not showing

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import {
+  auth,
+  createUserProfileDocument
+} from "./firebase/firebase.utils";
 
 import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -37,6 +40,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+    
     });
   }
 
